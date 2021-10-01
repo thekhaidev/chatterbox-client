b93e8d188adfc11dec1690f0e1f3025fb771f6c9
Khai Tran <kqtran917@yahoo.com>
2021-10-01 17:02:58 -0500

    Implemented  Parse.create method

--- client/scripts/parse.js
+++ client/scripts/parse.js
@@ -7,7 +7,17 @@ var Parse = {
   server: `https://app-hrsei-api.herokuapp.com/api/chatterbox/messages/${window.CAMPUS}`,
 
   create: function(message, successCB, errorCB = null) {
-    // TODO: send a request to the Parse API to save the message
+    // TODO: send a request to the Parse API to save the messag
+    $.ajax({
+      url: Parse.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: successCB,
+      error: errorCB || function(error) {
+        console.error('chatterbox: Failed to fetch messages', error);
+      }
+    });
   },
 
   readAll: function(successCB, errorCB = null) {
